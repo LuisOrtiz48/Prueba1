17f08952d0fe55cbb6a2fae29ad2f42b92e834aa
LuisOrtiz <luiscarlos7795@gmial.com>
2023-07-01 17:49:49 -0600

    Initial commit TEORIA DE LENGUAJES

---
+++ app/src/main/java/com/example/tlenguajes2023/Activityingresar.java
@@ -0,0 +1,82 @@
+package com.example.tlenguajes2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.tlenguajes2023.Configuracion.ConfigDB;
+import com.example.tlenguajes2023.Configuracion.SQLiteConnection;
+
+public class Activityingresar extends AppCompatActivity {
+
+    EditText id, nombres, apellidos, direccion, edad, correo;
+    Spinner genero;
+    Button btningresar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar);
+
+        id = (EditText) findViewById(R.id.txtid);
+        nombres = (EditText) findViewById(R.id.txtnomb);
+        apellidos = (EditText) findViewById(R.id.txtape);
+        genero = (Spinner) findViewById(R.id.cmdgenero);
+        direccion = (EditText) findViewById(R.id.txtdireccion);
+        edad = (EditText) findViewById(R.id.txtedad);
+        correo = (EditText) findViewById(R.id.txtcorreo);
+        btningresar = (Button) findViewById(R.id.btningresar);
+
+        btningresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Insertar_datos();
+            }
+        });
+
+    }
+
+    private void Insertar_datos()
+    {
+        SQLiteConnection conexion = new SQLiteConnection(this, ConfigDB.namebd, null, 1);
+        SQLiteDatabase db = conexion.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(ConfigDB.nombres,nombres.getText().toString());
+        values.put(ConfigDB.apellidos,apellidos.getText().toString());
+        values.put(ConfigDB.genero,genero.getSelectedItem().toString());
+        values.put(ConfigDB.direccion,direccion.getText().toString());
+        values.put(ConfigDB.edad,edad.getText().toString());
+        values.put(ConfigDB.correo,correo.getText().toString());
+
+        Long resultado = db.insert(ConfigDB.tblpersonas, ConfigDB.id, values);
+        if(resultado > 0)
+        {
+            Toast.makeText(getApplicationContext(), "Registro ingreso con exito", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Registro no se ingreso", Toast.LENGTH_SHORT).show();
+        }
+
+        db.close();
+
+        ClearScreen();
+
+    }
+
+    private void ClearScreen()
+    {
+        nombres.setText(ConfigDB.Enpty);
+        apellidos.setText(ConfigDB.Enpty);
+        direccion.setText(ConfigDB.Enpty);
+        edad.setText(ConfigDB.Enpty);
+        correo.setText(ConfigDB.Enpty);
+    }
+}
---
+++ app/src/main/java/com/example/tlenguajes2023/Configuracion/SQLiteConnection.java
@@ -0,0 +1,30 @@
+package com.example.tlenguajes2023.Configuracion;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SQLiteConnection extends SQLiteOpenHelper
+{
+
+    public SQLiteConnection(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+        /* Creacion de objetos de base de datos*/
+        sqLiteDatabase.execSQL(ConfigDB.CreateTBPersonas); // Creando la tabla de personas en SQLite
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)
+    {
+        /* Eliminando objetos de la base de datos*/
+        sqLiteDatabase.execSQL(ConfigDB.DropTBPersonas);
+        onCreate(sqLiteDatabase);
+    }
+}
